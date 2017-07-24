9989f5d458edc5fc24a080750060e82c95e05564
Vincze Dani <vdani@codingsans.com>
2017-07-24 10:03:18 +0200

    Gulpfile changes

--- gulpfile.js
+++ gulpfile.js
@@ -8,27 +8,36 @@ const reporter = ts.reporter.defaultReporter();
 
 const output = 'build';
 
+let process;
+
 gulp.task('start', function () {
-  nodemon({
+  process = nodemon({
     script: 'build/main.js',
     ext: 'js json',
-    watch: ['./build'],
+    watch: ['./invalid'],
     env: { 'NODE_ENV': 'development' }
-  })
+  });
 })
 
-gulp.task('transpile', function() {
-    const tsResult = gulp.src('src/**/*.ts')
-        .pipe(tsProject(reporter));
-
-    return merge([ // Merge the two output streams, so this task is finished when the IO of both operations is done.
-        tsResult.dts.pipe(gulp.dest(output)),
-        tsResult.js.pipe(gulp.dest(output))
-    ]);
+gulp.task('watch', ['transpile'], function() {
+  gulp.watch('src/**/*.ts', ['transpile', 'restart']);
 });
 
-gulp.task('watch', ['transpile'], function() {
-    gulp.watch('src/**/*.ts', ['transpile']);
+gulp.task('transpile', function() {
+  const tsResult = gulp.src('src/**/*.ts')
+    .pipe(tsProject(reporter));
+
+  return merge([ // Merge the two output streams, so this task is finished when the IO of both operations is done.
+    tsResult.dts.pipe(gulp.dest(output)),
+    tsResult.js.pipe(gulp.dest(output))
+  ]);
+});
+
+gulp.task('restart', function() {
+  if (!process) {
+    return;
+  }
+  process.emit('restart');
 });
 
 gulp.task('default', ['watch', 'start']);
